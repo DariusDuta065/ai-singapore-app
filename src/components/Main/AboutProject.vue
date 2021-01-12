a09babcde530d6ea326292864477a729ec053e3e
Darius Duta <dduta065@gmail.com>
2021-01-12 20:31:07 +0200

    Create About Project section

---
+++ src/components/Main/AboutProject.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row holder">
+    <div class="offset-md-3 col-md-6 container">
+      <h1>About the project</h1>
+      <p>
+        AI Singapore (AISG) is a national AI programme launched by the National
+        Research Foundation (NRF) to anchor deep national capabilities in
+        Artificial Intelligence (AI), focusing on four key pillars; research,
+        technology, innovation and makerspace.
+      </p>
+      <p>
+        This project is being instructed by AISG to collate a dataset of videos
+        to be used to train machine learning models to recognise specific
+        activities.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutProject",
+};
+</script>
+
+<style lang="scss" scoped>
+.holder {
+  padding: 50px 0;
+}
+</style>
