d7524d2793b7e4c175b7c99c93cdc15a689c940b
Darius Duta <dduta065@gmail.com>
2021-01-12 13:45:47 +0200

    Express server + compression

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const compression = require("compression");
+const history = require("connect-history-api-fallback");
+
+const app = express();
+app.use(compression());
+
+const staticFileMiddleware = express.static("dist");
+app.use(staticFileMiddleware);
+
+// Support history api
+app.use(
+  history({
+    index: "/dist/index.html",
+  })
+);
+
+// call for redirected requests
+app.use(staticFileMiddleware);
+
+const port = 8080;
+app.listen(port, function() {
+  console.log(`App listening on port ${port}.`);
+});
