d22a5dbb6d4f2e210c510d6f24e5dd2aa345c353
Darius Duta <dduta065@gmail.com>
2021-01-14 01:21:35 +0200

    Accordion component

---
+++ src/components/Common/Accordion.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="accordion-container">
+    <div>
+      <div class="accordion-header d-flex" :onClick="openAccordion">
+        <div class="accordion-header-title">
+          {{ title }}
+        </div>
+        <div class="accordion-header-caret ms-auto">
+          <img v-if="isOpen" :src="caretUpIcon" />
+          <img v-if="!isOpen" :src="caretDownIcon" />
+        </div>
+      </div>
+      <height-transition>
+        <div v-if="isOpen" class="accordion-content-container">
+          <div class="accordion-content">
+            <span v-html="answer" />
+          </div>
+        </div>
+      </height-transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeightTransition from "@c/Common/HeightTransition.vue";
+
+export default {
+  name: "Accordion",
+  components: {
+    HeightTransition,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    answer: {
+      type: String,
+      default: "",
+    },
+  },
+  data: function () {
+    return {
+      isOpen: false,
+      caretUpIcon: require("@img/svg/caret-up-fill.svg"),
+      caretDownIcon: require("@img/svg/caret-down-fill.svg"),
+    };
+  },
+  methods: {
+    openAccordion() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.accordion-container {
+  width: 100%;
+  margin: 0 auto;
+  max-width: 450px;
+  padding: 10px 0;
+
+  .accordion-header {
+    cursor: pointer;
+    padding: 12px 15px;
+    background: #eeeeee;
+
+    .accordion-header-title {
+      font-size: 1.1rem;
+    }
+  }
+
+  .accordion-content-container {
+    font-size: 1rem;
+    border: 1px solid #eeeeee;
+
+    .accordion-content {
+      padding: 20px 20px 12px 20px;
+    }
+  }
+}
+</style>
